762d6bc644c142c64375e7f2a9a69c769a1136e2
Paulo Melo <peimelo@gmail.com>
2020-07-21 16:43:16 -0300

    Restrict actions for unauthorized users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -10,22 +10,22 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     # Test to ensure response contains the correct email
     json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response['email']
+    assert_equal @user.email, json_response["email"]
   end
 
   test "should create user" do
-    assert_difference('User.count') do
+    assert_difference("User.count") do
       post api_v1_users_url,
-        params: { user: { email: 'test@test.org', password: '123456' } },
+        params: { user: { email: "test@test.org", password: "123456" } },
         as: :json
     end
     assert_response :created
   end
 
   test "should not create user with taken email" do
-    assert_no_difference('User.count') do
+    assert_no_difference("User.count") do
       post api_v1_users_url,
-        params: { user: { email: @user.email, password: '123456' } },
+        params: { user: { email: @user.email, password: "123456" } },
         as: :json
     end
     assert_response :unprocessable_entity
@@ -33,22 +33,29 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should update user" do
     patch api_v1_user_url(@user),
-      params: { user: { email: @user.email, password: '123456' } },
-      as: :json
+          params: { user: { email: @user.email } },
+          headers: { Authorization: JsonWebToken.encode(user_id: @user.id) },
+          as: :json
     assert_response :success
   end
 
-  test "should not update user when invalid params are sent" do
-    patch api_v1_user_url(@user),
-      params: { user: { email: 'bad_email', password: '123456' } },
-      as: :json
-    assert_response :unprocessable_entity
+  test "should forbid update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email } }, as: :json
+    assert_response :forbidden
   end
 
   test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete api_v1_user_url(@user), as: :json
+    assert_difference("User.count", -1) do
+      delete api_v1_user_url(@user),
+             headers: { Authorization: JsonWebToken.encode(user_id: @user.id) }, as: :json
     end
     assert_response :no_content
+  end
+
+  test "should forbid destroy user" do
+    assert_no_difference("User.count") do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
   end
 end
