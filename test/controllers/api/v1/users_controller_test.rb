fd34275ce56767cfbc8664ddc31564e64894c59a
Paulo Melo <peimelo@gmail.com>
2020-07-22 07:02:07 -0300

    Adds user serializer for customizing json output

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -8,9 +8,9 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
-    # Test to ensure response contains the correct email
+
     json_response = JSON.parse(self.response.body)
-    assert_equal @user.email, json_response["email"]
+    assert_equal @user.email, json_response["data"]["attributes"]["email"]
   end
 
   test "should create user" do
